0e13069a31bfddfe97de2aafef85b9ca1878499e
Filip Mastalerz <fmastalerz@griddynamics.com>
2019-09-17 12:20:52 +0200

    added class with 2 test methods

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,30 +9,31 @@ public class LoginTest {
     @Test
     void logUser() {
         String url = "https://conduit.productionready.io/api/users/login";
-        File userJson = new File("/home/ferm/IdeaProjects/conduitAnother/user.json");
+        File userJson = new File("user.json");
+        System.out.println(userJson.toString());
 
         given().
                 contentType("application/json").
-                body(userJson).
+                body(userJson).log().all().
                 when().
                 post(url).
                 then().
-                statusCode(200);
+                statusCode(200).log().all();
 
     }
 
     @Test
     void logUserAndCheckBody() {
         String url = "https://conduit.productionready.io/api/users/login";
-        File userJson = new File("/home/ferm/IdeaProjects/conduitAnother/user.json");
+        File userJson = new File("user.json");
 
         given().
                 contentType("application/json").
-                body(userJson).
+                body(userJson).log().all().
                 when().
                 post(url).
                 then().
-                body("user.id", equalTo(66512)).and().statusCode(200).log();
+                body("user.id", equalTo(66512)).and().statusCode(200).log().all();
 
     }
 }
