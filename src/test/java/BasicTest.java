ab0e8af7090e5679c708ef9438df0ee01120ecbc
Filip Mastalerz <fmastalerz@griddynamics.com>
2019-09-17 15:02:39 +0200

    added test method to get and update user profile

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -9,8 +9,9 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class BasicTest {
-    private static String url = "https://conduit.productionready.io/api/";
+    private static String url = "https://conduit.productionready.io/api";
     private static File userJson = new File("user.json");
+    private static File updatedUserJson = new File("updatedUser.json");
     private static String TOKEN;
 
     @BeforeAll
@@ -21,24 +22,23 @@ public class BasicTest {
                         contentType("application/json").
                         body(userJson).
                 when().
-                        post(url + "users/login").
+                        post(url + "/users/login").
                 then().
-                        body("user.id", equalTo(66512)).
-                and().
                         statusCode(200).
                         extract().
                         path("user.token");
     }
 
+    // test is using json with user body
     @Test
     @DisplayName("Authentication - log user, check his ID and status code")
-    void logUserCheckIdAndCode() {
+    void logUserCheckIdAndStatus() {
 
         given().
                 contentType("application/json").
                 body(userJson).
         when().
-                post(url + "users/login").
+                post(url + "/users/login").
         then().
                 body("user.id", equalTo(66512)).
         and().
@@ -47,22 +47,55 @@ public class BasicTest {
                 all();
     }
 
+    // test is using token in header
     @Test
-    @DisplayName("Get Current User - ")
-    void gettingCurrentUser() {
+    @DisplayName("Get Current User - check user email and status code")
+    void getUserCheckEmailAndStatus() {
 
         given().
                 contentType("application/json").
                 header("Authorization","Token " + TOKEN).
         when().
-                get(url + "user").
+                get(url + "/user").
         then().
                 body("user.email", IsEqual.equalTo("adam@mail.com")).
         and().
                 statusCode(200).
                 log().
                 all();
-
     }
 
+    // test is using parameters in URL
+    @Test
+    @DisplayName("Get Profile - check username and status code")
+    void getProfileCheckUsernameAndStatus() {
+        given().
+                contentType("application/json").
+                pathParam("username", "adam1234io").
+        when().
+                get(url + "/profiles/{username}").
+        then().
+                body("profile.username", IsEqual.equalTo("adam1234io")).
+        and().
+                statusCode(200).
+                log().
+                all();
+    }
+
+    @Test
+    @DisplayName("Update User")
+    void updateUserProfile() {
+        given().
+                header("Authorization", "Token " + TOKEN).
+                contentType("application/json").
+                body(updatedUserJson).
+        when().
+                put(url + "/user").
+        then().
+                body("user.bio", IsEqual.equalTo("I like to skateboard all day")).
+        and().
+                statusCode(200).
+                log().
+                all();
+    }
 }
