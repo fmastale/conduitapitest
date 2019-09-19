1d90527d9304cd880f41189c909d495f89e1af40
Filip Mastalerz <fmastalerz@griddynamics.com>
2019-09-19 10:09:33 +0200

    refactoring + clean up

--- src/test/java/basictestsv2/BasicTestsV2.java
+++ src/test/java/basictestsv2/BasicTestsV2.java
@@ -1,6 +1,7 @@
 package basictestsv2;
 
 import static io.restassured.RestAssured.given;
+import static junit.framework.Assert.assertEquals;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,7 +14,6 @@ import wrappers.UserResponseWrapper;
 
 public class BasicTestsV2 {
   private static String URL = "https://conduit.productionready.io/api";
-  private static String TOKEN;
 
   @Test
   @DisplayName("Authentication - log user, check his ID and status code, but with using ObjectMapper")
@@ -33,9 +33,6 @@ public class BasicTestsV2 {
         .body()
         .prettyPrint(), UserResponseWrapper.class);
 
-    TOKEN = response.userResponse.token;
-
-    System.out.println("Token: " + TOKEN);
-    System.out.println("Username: " + response.userResponse.username);
+    assertEquals("Expected and actual not same", "adam1234io", response.userResponse.username);
   }
 }
