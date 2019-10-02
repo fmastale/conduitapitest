a85b7cf9df910f6f8607cbe459f6de0c8a6a4364
fmastale <filip.mastalerz@gmail.com>
2019-10-02 21:17:39 +0200

    save commit

--- src/test/java/com/griddynamics/conduit/test/GetTagsTest.java
+++ src/test/java/com/griddynamics/conduit/test/GetTagsTest.java
@@ -3,7 +3,6 @@ package com.griddynamics.conduit.test;
 import com.griddynamics.conduit.helpers.Endpoint;
 import com.griddynamics.conduit.helpers.TestDataProvider;
 import com.griddynamics.conduit.helpers.TokenProvider;
-import com.griddynamics.conduit.jsons.Article;
 import com.griddynamics.conduit.jsons.Tags;
 import com.griddynamics.conduit.jsons.UserRequest;
 import io.qameta.allure.Description;
@@ -12,17 +11,12 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.griddynamics.conduit.helpers.Endpoint.ARTICLES;
-import static com.griddynamics.conduit.helpers.RequestSpecificationDetails.APPLICATION_JSON;
-import static com.griddynamics.conduit.helpers.RequestSpecificationDetails.AUTHORIZATION;
 
 @Epic("Smoke tests")
 @Feature("Get Tags")
@@ -31,18 +25,12 @@ public class GetTagsTest {
     private static TestDataProvider testDataProvider = new TestDataProvider();
     private static UserRequest user = testDataProvider.getTestUser();
 
-
-    // 3. get list of tags
-    // 4. check if our tag is on the list
-
     @BeforeAll
     static void prepareEnvironment() {
         RestAssured.baseURI = Endpoint.BASE_URI.get();
 
         TokenProvider tokenProvider = new TokenProvider();
         token = tokenProvider.getTokenForUser(user);
-
-
     }
 
     @Severity(SeverityLevel.NORMAL)
@@ -51,18 +39,6 @@ public class GetTagsTest {
     @DisplayName("Get tags list, check length")
     void getTagsCheckLength() {
         // GIVEN
-        /* I've tried to create article with a given tag and check it on the list, but list don't contain all the tags!
-        String[] tags = new String[]{"FancyTestTag", "AndAnother"};
-        Article article = new Article("This is article title", "This is description", "This is body", tags);
-
-        RequestSpecification articleRequestSpecification = RestAssured.given()
-                .contentType(APPLICATION_JSON.getDetails())
-                .header(AUTHORIZATION.getDetails(), token)
-                .body(article);
-        Response response = articleRequestSpecification.post(ARTICLES.get());
-        response.prettyPrint();*/
-
-
         RequestSpecification requestSpecification = RestAssured.given();
 
         // WHEN
